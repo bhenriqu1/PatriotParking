34401775914fb3ccb0c6d34c8dc2e7c8f0733ae7
minhyukko <57718746+minhyukko@users.noreply.github.com>
2019-11-17 22:29:11 -0500

    Update WrongCarpoolNumberException.java

--- WrongCarpoolNumberException.java
+++ WrongCarpoolNumberException.java
@@ -2,5 +2,6 @@ public class WrongCarpoolNumberException extends Exception {
 
    public WrongCarpoolNumberException() {
       super();
+      System.out.println("Please enter a positive integer.");
    }
 }
